20ff8efa97031ff863b62b781682fba2afbcf1e6
Steve Kaats <stevenakaats@gmail.com>
2025-01-27 07:40:34 -0800

    Added About Us tagline

--- frontend/src/pages/AboutPage/AboutPage.jsx
+++ frontend/src/pages/AboutPage/AboutPage.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router';
-import * as postService from '../../services/jobService';
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import * as postService from "../../services/jobService";
 
 export default function AboutPage() {
-  const [content, setContent] = useState('');
+  const [content, setContent] = useState("");
 
   const navigate = useNavigate();
 
@@ -11,7 +11,7 @@ export default function AboutPage() {
     evt.preventDefault();
     try {
       const post = await postService.create(content);
-      navigate('/posts');
+      navigate("/posts");
     } catch (err) {
       console.log(err);
     }
@@ -20,6 +20,20 @@ export default function AboutPage() {
   return (
     <>
       <h2>About Us</h2>
+      <h3>Family-Owned and Operated for 50 Years</h3>
+      <p>
+        At American Roofing and Waterproofing, we take pride in being a trusted
+        name in roofing for both residential and commercial properties across
+        California. For over five decades, our family-owned and operated
+        business has been committed to delivering exceptional craftsmanship,
+        reliable service, and lasting value to every project we undertake.
+        Serving areas from San Diego to Santa Rosa, our dedication to quality
+        and customer satisfaction has allowed us to build long-lasting
+        relationships with our clients and remain a staple in the community. As
+        we look toward the future, we continue to uphold the values that have
+        made us a leader in the roofing industry—integrity, trust, and
+        excellence.
+      </p>
     </>
   );
 }
